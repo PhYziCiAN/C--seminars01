430e1eeaacbea339d8a1da64079c18fe3ff4e2e2
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-11-06 23:20:58 +0300

    Solved Task01

--- Seminar3/Task01/Program.cs
+++ Seminar3/Task01/Program.cs
@@ -1,25 +1,8 @@
-﻿// number square, Math.Sqrt(Double) Method
-// double Sqrt (double d);
-// Console.Write ("Please input your number: "); // ask for input
-// double d = Convert.ToDouble(Console.ReadLine()); 
+﻿//number in square
+Console.Write ("Please input your number: "); // ask for input
+double asd = Convert.ToDouble(Console.ReadLine()); 
+asd = asd * asd;
+Console.WriteLine(asd);
 
-// Console.WriteLine(Sqrt);
 
-//double Sqrt (double d);
 
-// Tuple<string, double>[] areas =
-//                { Tuple.Create("Sitka, Alaska", 2870.3),
-//                  Tuple.Create("New York City", 302.6),
-//                  Tuple.Create("Los Angeles", 468.7),
-//                  Tuple.Create("Detroit", 138.8),
-//                  Tuple.Create("Chicago", 227.1),
-//                  Tuple.Create("San Diego", 325.2) };
-
-// Console.WriteLine("{0,-18} {1,14:N1} {2,30}\n", "City", "Area (mi.)",
-//                   "Equivalent to a square with:");
-
-// foreach (var area in areas)
-//   Console.WriteLine("{0,-18} {1,14:N1} {2,14:N2} miles per side",
-//                     area.Item1, area.Item2, Math.Round(Math.Pow(area.Item2), 2));
-
-Console.WriteLine(Math.Round(3.444m, 2));
