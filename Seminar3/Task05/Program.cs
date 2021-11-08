d1bb377c2ad886a20a719359e9c4e9fa7460e5a1
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-11-08 22:28:17 +0300

    New solutions

---
+++ Seminar3/Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿int Function(int a)
+{
+    int y = a * a ;
+    return y;
+}
+Console.WriteLine("Введите число перменную a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваш ответ: " + Function(a));
