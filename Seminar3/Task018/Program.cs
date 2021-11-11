d097d1e4abec2ecd043331244194d72ea48ef458
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-11-11 22:33:05 +0300

    Yet some solutions

---
+++ Seminar3/Task018/Program.cs
@@ -0,0 +1,6 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool x = false;
+bool y = false;
+bool f1 = !(x||y);// OR operand
+bool f2 = !x && !y; // AND operanf
+Console.WriteLine(f1==f2); 
