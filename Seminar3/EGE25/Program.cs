f28b525b0b9284e0806e04d7a043384ddbc20b33
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-11-15 22:12:21 +0300

    added 12, 25, 29

---
+++ Seminar3/EGE25/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int CountDiv(int n)
+{
+    List<int> list = new List<int>();
+    list.Add(1);
+    int k=0;
+    for (int i=2; i<n; i++)
+    if (n%i == 0) 
+        {k++;
+        list.Add(i);
+    return list;
+}
+
+List<int> list;
+for(int i=210235; i<=210300; i++)
+{
+    list = CountDiv(i);
+    if(List.Count==4)
+    {
+        Console.WriteLine(i+ ":");
+        for(int j=0; j<list.Count; j++);
+    }
+}
---
+++ Seminar3/Task012/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру 3-значного числа
+
+int a = 258;
+
+int b = a % 100 / 10;
+int c = a / 100;
+int d = a % 10;
+
+Console.WriteLine(a +"\n"+ c +""+ b);
+Console.WriteLine(c);
+Console.WriteLine(b);
+Console.WriteLine(d);
