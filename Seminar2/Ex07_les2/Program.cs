2c893e4f1e4642c1acc9f3cb616c53fc1664187c
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-11-19 11:21:28 +0300

    some new tasks solved

--- Seminar2/Ex07_les2/Program.cs
+++ Seminar2/Ex07_les2/Program.cs
@@ -3,32 +3,32 @@
 
 int[] a; //описание массива
 a=new int[10]; //создание массива
-int i;
+int i = 0;
 
 //Первый способ
 i=0;
-while(i<10)
+while(i<9)
 {
     a[i]=0;
-    ++i;
+    i++;
 Console.WriteLine(a[i]);
 }
 
 //Второй способ
-for(int j=0; j<10; ++j)
-{
-    a[j]=0;
-}
-for(i=0; i<10; ++i)
-{
-    a[i]=0;
-}
-Console.WriteLine(a[i]);
+// for(int j=0; j<10; ++j)
+// {
+//     a[j]=0;
+// }
+// for(i=0; i<10; ++i)
+// {
+//     a[i]=0;
+// }
+// Console.WriteLine(a[i]);
 
 // Ввод массива с клавиатуры
 for(int j=0;j<10;++j)
 {
-    string s=Console.ReadLine();
+    string s = Console.ReadLine();
     a[j]=Convert.ToInt32(s);
 }
 for(i=0;i<10;++i)
--- Seminar3/Task03/Program.cs
+++ Seminar3/Task03/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] a = {Monday, Tuesday, Wednesday, Thursday, Saturday, Sunday}; //описание массива
+//a=new int[10]; //создание массива
+//int i = 7;
+Console.WriteLine(a[i]+" ");
+
+// for(int j=0;j<7;++j)
+// {
+//     string s = Console.ReadLine();
+//     a[j]=Convert.ToInt32(s);
+// }
+// for(i=0;i<10;++i)
+// {
+//     Console.WriteLine(a[i]+" ");
+// }
