0e942426c56b7782621e6c9f7c660acba865e5ff
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-11-22 22:28:30 +0300

    Solved some conflicts

--- Seminar2/Ex07_les2/Program.cs
+++ Seminar2/Ex07_les2/Program.cs
@@ -28,7 +28,7 @@ Console.WriteLine(a[i]);
 // Ввод массива с клавиатуры
 for(int j=0;j<10;++j)
 {
-    string s = Console.ReadLine();
+    string s = Console.ReadLine()!;
     a[j]=Convert.ToInt32(s);
 }
 for(i=0;i<10;++i)
--- Seminar3/EGE25/Program.cs
+++ Seminar3/EGE25/Program.cs
@@ -1,28 +1,59 @@
 ﻿
 
-int CountDiv(int n)
-{
-    List<int> list = new List<int>();
-    list.Add(1);
-    int k=0;
-    for (int i=2; i<n; i++)
-        if (n%i == 0) 
-        {
-            k++;
-            list.Add(i);
-        }
-            return list;
-}
-List<int> list;
-for(int i=210235; i<=210300; i++)
-    {
-        list = CountDiv (i);
-        if (list.Count != 4)
-        {
-            continue;
-        }
-        for (int j = 0; j < list.Count; j++) ;
-        Console.WriteLine(i + ":");
-    }
+// int CountDiv(int n)
+// {
+//     List<int> list = new List<int>();
+//     list.Add(1);
+//     int k=0;
+//     for (int i=2; i<n; i++)
+//         if (n%i == 0) 
+//         {
+//             k++;
+//             list.Add(i);
+//         }
+//             return list;
+// }
+// List<int> list;
+// for(int i=210235; i<=210300; i++)
+//     {
+//         list = CountDiv (i);
+//         if (list.Count != 4)
+//         {
+//             continue;
+//         }
+//         for (int j = 0; j < list.Count; j++) ;
+//         Console.WriteLine(i + ":");
+//     }
 
-Console.WriteLine(CountDiv(5));
+// Console.WriteLine(CountDiv(5));
+
+int[] CreateIntArray(int minValue, int maxValue)
+{
+    int[] tempArr = new int[maxValue-minValue+1];
+    for (int i = 0; i < tempArr.Length; i++) tempArr[i] = minValue++;
+    return tempArr;
+}
+
+int[] TakeRatioFor(int compareNumber)
+{
+    int count = 1;
+    int[] plusArr = new int[count];
+    int num = 2;
+    for(int i = 0; i < compareNumber; i++, num++)
+    {
+        if (compareNumber%(num) == 0) {Array.Resize(ref plusArr, count); plusArr[count-1] = num; count++;}
+    }
+    return plusArr;
+}
+
+void PrintArray(int[] inputArr)
+{
+    foreach (int input in inputArr) Console.Write($"{input} "); Console.WriteLine();
+}
+int[] newArr  = CreateIntArray(210235,210300);
+// int[] newArr = CreateIntArray(10,16);
+foreach (int numbArr in newArr) 
+{
+    int[] x = TakeRatioFor(numbArr);
+    if (x.Length == 5) PrintArray(x);
+}
