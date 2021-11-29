fa3676f38325575bfc88aae94652fa5425896367
Andrei Zhernakov <a.s.jernakov@gmail.com>
2021-11-29 22:23:40 +0300

    added some methods

--- C#praktikum/Ex002/Program.cs
+++ C#praktikum/Ex002/Program.cs
@@ -30,21 +30,85 @@ bool MakeMove(int SecretNumber, int AttemptpsCount) -  метод, в котор
 ход, в результате метод возвращает либо true (угадано чсло, остановка игры), 
 либо false (пользователь не угадал).
 */
-int SecretNumber = createNumber(1,100);
-// int PlayersNumber = requestNumber ();
+int SecretNumber = createNumber(1, 100); // int PlayersNumber = requestNumber ();
 
 int createNumber(int leftBound, int rightBound)
 {
-    int n = new Random().Next(leftBound, rightBound);
-    return n;
+    return new Random().Next(leftBound, rightBound + 1);
 }
 Console.WriteLine(SecretNumber); //test
 
+int leftBound = 1; //test of method
+int rightBound = 100;
+int result112 = createNumber(leftBound, rightBound);
+    if (result112 > leftBound && result112 <= rightBound)
+    {
+        Console.WriteLine("Correct");
+    }
+    else
+    {
+        Console.WriteLine("Not correct");
+    }
 int requestNumber()
 {
-    Console.WriteLine("Guess number from 1 to 100");
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
+    int requestNumber = 0;
+    while (true)
+    {
+        Console.WriteLine("Guess number from 1 to 100");
+        if (int.TryParse((Console.ReadLine()), out int result))
+        {
+            if (result > 0)
+            {
+                requestNumber = result;
+                Console.Clear();
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Number did not resolved");
+        }
+    }
+    return requestNumber;
 }
 int temp = requestNumber();
 Console.WriteLine(temp); //test
+
+bool MakeMove (int SecretNumber, int AttemptsCount)
+{
+    int PlayersNumber = requestNumber();
+    if (PlayersNumber == SecretNumber)
+        {
+            Console.WriteLine($"You guess number ");
+            return true;
+        }
+    else
+        {
+            AttemptsCount--;
+            if (AttemptsCount >0)
+            {
+                if (PlayersNumber != SecretNumber)
+                {
+                    Console.WriteLine("You do not guess number");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("There is no attempts left");
+                }
+            }
+        }           
+}
+
+int reqNumb0 = 0;
+int AttemptsCount = 6;
+for (int i = 0; i < 5; i++)
+{
+    reqNumb0 = requestNumber();
+    bool reqNumb1 = MakeMove (int SecretNumber, int AttemptsCount);
+    if (reqNumb1 == true)
+    {
+        Console.WriteLine("Good-bye");
+        break;
+    }
+}
