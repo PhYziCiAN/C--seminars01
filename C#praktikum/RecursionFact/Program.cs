99bee0ddff5b63cf1f0d34666375297d11d98298
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-12-11 03:20:10 +0300

    recursion

---
+++ C#praktikum/RecursionFact/Program.cs
@@ -0,0 +1,11 @@
+﻿// Factorial as a recursion example
+
+double Factorial (double n)
+{
+    if (n == 1) return 1;
+     return  n * Factorial (n - 1);
+}
+for (double i = 1; i < 100; i++)
+{
+    Console.WriteLine ($"{i!} = {Factorial(i)}");
+}
--- Seminar3/Task02/Program.cs
+++ Seminar3/Task02/Program.cs
@@ -1,13 +1,33 @@
 ﻿//output maximal and mininmal of 2 integers
-int a = 3;
-int b = 6;
-if ( a > b)
+// int a = 3;
+// int b = 6;
+// if ( a > b)
+// {
+//     Console.WriteLine("Max is: "+a);
+//     Console.WriteLine("Min is: "+b);
+// }
+// else
+// {
+//     Console.WriteLine("Max is: "+b);
+//     Console.WriteLine("Min is: "+a);
+// }
+
+int PrintMax(int a, int b)
 {
-    Console.WriteLine("Max is: "+a);
-    Console.WriteLine("Min is: "+b);
+    
+    if (a > b) return a;
+    else return b;
+        
 }
-else
+int PrintMin(int a, int b)
 {
-    Console.WriteLine("Max is: "+b);
-    Console.WriteLine("Min is: "+a);
+    
+    if (a < b) return a;
+    else return b;
+        
 }
+
+Console.WriteLine($"Max is: {PrintMax (5,6)} \nMin is: {PrintMin(5,6)}");
+
+// int n = PrintMax(6,5);
+// Console.WriteLine("Max is:" +n);
