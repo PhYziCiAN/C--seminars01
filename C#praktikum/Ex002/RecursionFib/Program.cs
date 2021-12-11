5b506c3123fc98988dd1c49be8b4139201ab2056
Zhernakov Andrei <a.s.jernakov@gmail.com>
2021-12-11 03:29:10 +0300

    stopwatch

--- C#praktikum/Ex002/RecursionFib/Program.cs
+++ C#praktikum/Ex002/RecursionFib/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System.Diagnostics;
-using System;
-System.Diagnostics.Stopwatch stopwatch = new Stopwatch();
+Stopwatch stopwatch = new Stopwatch();
 stopwatch.Start();
-double Fibonacci (double n)
+double Fibonacci(double n)
 {
     if (n == 1 || n == 2) return 1;
-    else return Fibonacci(n-1) + Fibonacci (n-2);
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 for (double i = 1; i < 41; i++)
 {
-    Console.WriteLine ($"{i!} = {Fibonacci (i)}");
+    Console.WriteLine($"{i!} = {Fibonacci(i)}");
 }
 stopwatch.Stop();
 TimeSpan timeTaken = stopwatch.Elapsed;
 string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
+Console.WriteLine(foo);
